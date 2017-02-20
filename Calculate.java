a3b5c0b313ba3350b10b2aa41c653f10911d13d3
YuriZuzia <teanjam@ya.ru>
2017-02-20 20:13:45 +0500

    Calculate create

--- Calculate.java
+++ Calculate.java
@@ -1,9 +1,18 @@
+/* 
+ 20.02.2017
+ * Зюзя Ю.В
+ * Калькулятор
+ */
 public class Calculate {
 	public static void main(String[] arg){
 		System.out.println("Calculate...");
 		int first = Integer.valueOf(arg[0]);
 		int second = Integer.valueOf(arg[1]);
-		int sum = first + second;
-		System.out.println(String.format("%s + %s = %s", first, second, sum));
+		System.out.println(String.format("%s + %s = %s", first, second, sum(first, second)));
+	}
+	
+
+	public static int sum(int first, int second){
+		return first+second;
 	}
 }
