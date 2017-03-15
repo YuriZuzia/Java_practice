41bd2ffd5896828719bcc9e9fe9f22660f17413c
YuriZuzia <teanjam@ya.ru>
2017-03-15 22:02:54 +0500

    task 4086

--- Chapter_001/src/main/java/ru/job4j/Calculate.java
+++ Chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,11 +1,10 @@
+package ru.job4j;
 /**
  *Class Calculate - вывод "Привет, мир!"
  *@author YuZuzia (mailto: zuziayv@ya.ru)
  *@version %i%, %G%
  *@since 15.03.2017 
 */
-package ru.job4j;
-
 public class Calculate{
 	public static void main(String[] args){
 		System.out.println("Hello snowboard!");
---
+++ Chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *Class Calculate - вывод "Привет, мир!"
+ *@author YuZuzia (mailto: zuziayv@ya.ru)
+ *@version $Id$
+ *@since 15.03.2017 
+*/
+package ru.job4j;
