b0760defde3e434ed7bb28380d488be863bcb3f4
YuriZuzia <teanjam@ya.ru>
2017-03-16 18:58:02 +0500

    task 4086

--- Chapter_001/src/main/java/ru/job4j/package-info.java
+++ Chapter_001/src/main/java/ru/job4j/package-info.java
@@ -1,7 +1,8 @@
 /**
- *Class Calculate - вывод "Привет, мир!"
  *@author YuZuzia (mailto: zuziayv@ya.ru)
  *@version $Id$
  *@since 15.03.2017 
+ *Class Calculate - вывод "Привет, мир!"
+ * метод main - вывод на экран строки
 */
 package ru.job4j;
